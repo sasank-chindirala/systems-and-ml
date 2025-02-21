d4d9e6939dd844bdf66efa7b5a191a630df11ef3
sasank-chindirala <sasankchindirala@gmail.com>
2025-02-20 21:46:44 -0500

    - multiple cnn models tested and added support to plot losses and accuracies of each model against epochs

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
+import ssl
+import matplotlib.pyplot as plt
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
-class Net(nn.Module):
+class Net1(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(Net1, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
@@ -34,8 +37,68 @@ class Net(nn.Module):
         return output
 
 
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 32, 3, 1)
+        self.conv3 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(7744, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+class Net3(nn.Module):
+    def __init__(self):
+        super(Net3, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
+    train_loss = 0
+    correct = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -43,12 +106,19 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+        train_loss += loss.item()
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
             if args.dry_run:
                 break
+
+    train_loss /= len(train_loader.dataset)
+    train_accuracy = 100. * correct / len(train_loader.dataset)
+    return train_loss, train_accuracy
 
 
 def test(model, device, test_loader):
@@ -69,6 +139,33 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+
+def plot_results(training_losses, testing_losses, training_accuracies, testing_accuracies, model_name):
+    epochs = range(1, len(training_losses) + 1)
+
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, training_losses, label="Training Loss")
+    plt.plot(epochs, testing_losses, label="Testing Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title(f"{model_name} - Loss vs. Epoch")
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, training_accuracies, label="Training Accuracy")
+    plt.plot(epochs, testing_accuracies, label="Testing Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title(f"{model_name} - Accuracy vs. Epoch")
+    plt.legend()
+
+    plt.show()
+
 
 def main():
     # Training settings
@@ -77,8 +174,8 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--epochs', type=int, default=5, metavar='N',
+                        help='number of epochs to train (default: 5)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
@@ -120,17 +217,41 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    def train_model(model, train_loader, test_loader, optimizer):
+        train_losses = []
+        test_losses = []
+        train_accuracies = []
+        test_accuracies = []
+        for epoch in range(1, args.epochs + 1):
+            train_loss, train_accuracy = train(args, model, device, train_loader, optimizer, epoch)
+            test_loss, test_accuracy = test(model, device, test_loader)
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
+            train_accuracies.append(train_accuracy)
+            test_accuracies.append(test_accuracy)
+            scheduler.step()
+        return train_losses, test_losses, train_accuracies, test_accuracies
 
+    model1 = Net1().to(device)
+    optimizer = optim.Adadelta(model1.parameters(), lr=args.lr)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
-        scheduler.step()
+    results1 = train_model(model1, train_loader, test_loader, optimizer)
+    plot_results(*results1, model_name="Model1")
 
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+    model2 = Net2().to(device)
+    optimizer = optim.Adadelta(model2.parameters(), lr=args.lr)
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    results2 = train_model(model2, train_loader, test_loader, optimizer)
+    plot_results(*results2, model_name="Model2")
+
+    model3 = Net3().to(device)
+    optimizer = optim.Adadelta(model3.parameters(), lr=args.lr)
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    results3 = train_model(model3, train_loader, test_loader, optimizer)
+    plot_results(*results3, model_name="Model3")
+
+    # if args.save_model:
+    #     torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == '__main__':
